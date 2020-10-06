66ffdd18fb6c1ec0fc0cca4d402f83fe9fcd43dc
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-10-06 23:03:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -16,6 +16,6 @@ def calcul_volume_et_masse (axe_a, axe_b, axe_c, masse_volumique) :
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     données = input("Veuillez entre vos trois demi-axes et la masse volumique, séparés par une virgule :")
-    print(co)
+    print(calcul_volume_et_masse(données))
     
     pass
