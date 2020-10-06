7a6270fd1fa8e8ccc2d9188d37876b5d2043d100
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-10-06 22:53:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,6 +8,7 @@ import math
 # TODO: Définissez vos fonction ici
 def calculation_volume_and_mass (axe_a, axe_b, axe_c, masse_volumique) :
     Volume = 4/3 * math.pi *axe_a * axe_b * axe_c
+    
 
 
 
