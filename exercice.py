bf93bcc1abcce284ee3438310c0d18cac2d79084
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-10-06 23:01:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,5 +15,5 @@ def calcul_volume_et_masse (axe_a, axe_b, axe_c, masse_volumique) :
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    données = input("Veuillez entre vos trois demis-ax")
+    données = input("Veuillez entre vos trois demi-axes et ")
     pass
